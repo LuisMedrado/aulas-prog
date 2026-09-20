ead36c3edb6733935a00431170be8505d055411a
LuisMedrado <luplfmc@gmail.com>
2026-09-20 15:45:46 -0300

    interface da LoadingScreen e StartScreen prontas

---
+++ projeto-gameplay/projetoGameplay/src/app/_layout.tsx
@@ -0,0 +1,30 @@
+import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
+import {
+    Rajdhani_700Bold,
+    useFonts,
+} from '@expo-google-fonts/rajdhani';
+import { Slot } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Rajdhani_700Bold,
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Slot />;
+}
